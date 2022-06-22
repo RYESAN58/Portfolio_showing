d9a37343da9e2e0131486b9da2d07a7ad1f82e61
Verified ✔️ <ryesan58@gmail.com>
2022-06-22 17:41:20 -0400

    portfolio 2022

---
+++ portfolio/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import Saveicon from '@mui/icons-material/Save';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid'
+import Profile from './components/profile';
+import Header from './components/header';
+import Porfolio from './components/porfolio';
+import Footer from './components/footer';
+import Resume from './components/resume';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+
+
+function App() {
+  return (
+    <Container>
+      <Grid container>
+        <Grid item md={4} lg={3}>
+          <Profile/>
+        </Grid>
+          <Grid item xs>
+            <Header/>
+            <BrowserRouter>
+              <Routes>
+                <Route path='/' element={<Porfolio/>}/>
+                <Route path='/resume' element={<Resume/>}/>
+              </Routes>
+            </BrowserRouter>
+            <Footer/>
+          </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+
+const Profile = () => {
+  return (
+    <div>
+      <Typography>
+        Ryan
+      </Typography>
+    </div>
+  )
+}
+
+export default Profile
