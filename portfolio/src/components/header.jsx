996fef3a8e9c726090dde45359d5fa5affdcf664
Verified ✔️ <ryesan58@gmail.com>
2022-06-26 02:16:59 -0400

    finished  header component

--- portfolio/src/components/header.jsx
+++ portfolio/src/components/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
+import "../App.css"
 import 'bootstrap/dist/css/bootstrap.css';
 import {
-  Button,
   Nav,
   Navbar,
   Container,
@@ -9,33 +9,54 @@ import {
   NavLink
 } from 'react-bootstrap'
 import HomeIcon from '@mui/icons-material/Home';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import Button from '@mui/material/Button'
+import TelegramIcon from '@mui/icons-material/Telegram';
 import {Link} from 'react-router-dom'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Header = (props) => {
-  const pathName = props?.location?.pathname
+  const [pathName, setPath] = useState('')
+  const [dummy, setDum] = useState(true)
+
+  useEffect(() => setPath(window.location.pathname), [dummy])
+
+
+  console.log(pathName)
   return (
-    <div>
       <Navbar expand='lg' sticky='top' className='header'>
-        <Container>
-        <NavLink as={Link} to='/'>
-          <NavbarBrand>
-            <HomeIcon/>
+        <NavLink as={Link} to='/' className='header_home'>
+          <NavbarBrand >
+            <HomeIcon className='ICON'/>
           </NavbarBrand>
         </NavLink>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{marginRight: "6px"}}/>
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav>
-            <NavLink as={Link} to='/' className={pathName == '/' ? "header_link_active": "header_link"}>
+          <Nav className='header_left'>
+            <NavLink as={Link} to='/' onClick={ () => setDum(!dummy)} className={pathName == '/' ? "header_link_active": "header_link"} >
               Porfolio
             </NavLink>
-            <NavLink as={Link} to='/resume' className={pathName == '/resume' ? "header_link_active": "header_link"}>
+            <NavLink as={Link} to='/resume' onClick={ () => setDum(!dummy)} className={pathName == '/resume' ? "header_link_active": "header_link"}>
               Resume
             </NavLink>
           </Nav>
-          </Navbar.Collapse>
-          </Container>
+          <div className='header-right'>
+            <a href='https://github.com/RYESAN58'>
+              <GitHubIcon className='iconss'/>
+            </a>
+            <a href='https://www.linkedin.com/in/ryan-santiago-4111a4166/'>
+              <LinkedInIcon className='iconss'/>
+            </a>
+            <a href='https://twitter.com/__worst'>
+              <TwitterIcon className='iconss'/>
+            </a>
+            <Button variant="contained" className='buttons' style={{margin:'0px'}}>Hire Me!<TelegramIcon/></Button>
+          </div>
+        </Navbar.Collapse>
       </Navbar>
-    </div>
   )
 }
 
