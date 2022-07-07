a4540d3c7c44b21087ebc6292d73f14d20d5f2c0
Verified ✔️ <ryesan58@gmail.com>
2022-07-07 00:39:12 -0400

    Update contact.jsx

--- portfolio/src/components/contact.jsx
+++ portfolio/src/components/contact.jsx
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button'
 import emailjs from 'emailjs-com'
 import "../App.css"
 import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography';
 
 
 const Contact = () => {
@@ -25,7 +26,7 @@ const Contact = () => {
     if(e.target.value.length < 1) {
       setNameError("Name is Required!");
   } else if(e.target.value.length < 3) {
-      setNameError("Name must be 3 characters or longer");
+      setNameError("Must be 3 characters");
   } else {
       setNameError("");
   }
@@ -99,11 +100,11 @@ const Contact = () => {
               }
               <TextField type="email" label="Your email address" varaint= 'outlined' onChange={handleEmail} name='email'/>
             </Grid>
-            <Grid xs={11} style={{marginTop:"20px", marginLeft:"15px"}} >
+            <Grid xs={12} style={{marginTop:"20px", marginLeft:"15px"}} >
               <TextField  type= 'text' variant= 'outlined' label="Message me" onChange={handleMessage} name='message'  fullWidth multiline rows={4}/>
             </Grid>
-            <Grid xs={11} style={{marginTop:"20px"}}>
-              <Button variant='contained' color='primary' type='submit' className='ml' style={{marginLeft:"17px", display:`${display}`}}>Send Message</Button>
+            <Grid xs={12} style={{marginTop:"20px"}}>
+              <Button variant='contained' color='primary' type='submit' className='ml buttons' style={{marginLeft:"17px", display:`${display}`}}>Send Message</Button>
             </Grid>
           {
             emailSent ?
@@ -117,6 +118,31 @@ const Contact = () => {
           }
           </Grid>
         </Box>
+      </Grid>
+      <Grid container lg={5} style={{marginLeft:"10px"}} className="Address-con">
+        <Grid item className='grid_title mb-30'xs={12} style={{height:"22px"}}>
+          <span style={{width:"105px"}}></span>
+          <h5 className='section_text'> Contact Info </h5>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container style={{marginBottom:"500px"}} spacing={5}>
+            <Grid item>
+              <Typography className='contact_info'>
+                <span className='Types'>Location:</span> New York City
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography className='contact_info'>
+                <span className='types'>Phone:</span> +13476819169
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography className='contact_info'>
+                <span className='types'>Email:</span> Ryesan58@gmail.com
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
